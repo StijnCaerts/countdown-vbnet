b3f0d0d25dd8f4dfb0b96dc0c8e32dced3914d22
Stijn Caerts <stijncaerts@gmail.com>
2014-12-27 20:50:26 +0100

    project created

---
+++ Countdown/Form1.Designer.vb
@@ -0,0 +1,56 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class FormCountdown
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.components = New System.ComponentModel.Container()
+        Dim resources As System.ComponentModel.ComponentResourceManager = New System.ComponentModel.ComponentResourceManager(GetType(FormCountdown))
+        Me.LabelTime = New System.Windows.Forms.Label()
+        Me.Timer1 = New System.Windows.Forms.Timer(Me.components)
+        Me.SuspendLayout()
+        '
+        'LabelTime
+        '
+        resources.ApplyResources(Me.LabelTime, "LabelTime")
+        Me.LabelTime.BackColor = System.Drawing.Color.Transparent
+        Me.LabelTime.Name = "LabelTime"
+        '
+        'Timer1
+        '
+        '
+        'FormCountdown
+        '
+        resources.ApplyResources(Me, "$this")
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.BackColor = System.Drawing.SystemColors.Control
+        Me.Controls.Add(Me.LabelTime)
+        Me.Cursor = System.Windows.Forms.Cursors.Default
+        Me.Name = "FormCountdown"
+        Me.WindowState = System.Windows.Forms.FormWindowState.Maximized
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+    Friend WithEvents LabelTime As System.Windows.Forms.Label
+    Friend WithEvents Timer1 As System.Windows.Forms.Timer
+
+End Class
---
+++ Countdown/Form1.vb
@@ -0,0 +1,50 @@
+﻿Public Class FormCountdown
+    Dim timeLeft As Integer = DateDiff(DateInterval.Second, Now, CDate("01 Jan 2015 00:00:00"))
+
+    Private Sub FormCountdown_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        updateLabelTime()
+        FormCountdown_Resize()
+        Timer1.Interval = 1000
+        Timer1.Start()
+    End Sub
+
+
+    Private Sub Timer1_Tick(sender As Object, e As EventArgs) Handles Timer1.Tick
+        If timeLeft > 0 Then
+            timeLeft -= 1
+            updateLabelTime()
+        Else
+            Timer1.Stop()
+            newYear()
+        End If
+    End Sub
+
+    Private Sub updateLabelTime()
+        Dim hours As Integer
+        Dim minutes As Integer
+        Dim seconds As Integer
+
+        hours = timeLeft \ 3600
+        minutes = (timeLeft - hours * 3600) \ 60
+        seconds = timeLeft - hours * 3600 - minutes * 60
+
+        LabelTime.Text = Format(hours, "00") & " : " & Format(minutes, "00") & " : " & Format(seconds, "00")
+    End Sub
+
+    Private Sub newYear()
+        LabelTime.Text = "Happy New Year!"
+        FormCountdown_Resize()
+
+        LabelTime.ForeColor = Color.DarkSlateBlue
+        Me.BackColor = Color.Gold
+    End Sub
+
+    Private Sub FormCountdown_Resize() Handles Me.Resize
+        If Me.WindowState <> FormWindowState.Minimized Then
+            LabelTime.Font = New Font("Trebuchet MS", (Me.ClientSize.Width / LabelTime.Text.Length) * 1.125)
+            LabelTime.Left = (Me.ClientSize.Width - LabelTime.Width) / 2
+            LabelTime.Top = (Me.ClientSize.Height - LabelTime.Height) / 2
+        End If
+        
+    End Sub
+End Class
